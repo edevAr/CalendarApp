b82f5ca8d053bdd1bafc45061c56995cf5e5e201
Eden Guzman Andia <eden.guzman@niceincontact.com>
2024-05-09 22:22:24 -0400

    Adding localizations to the project

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,7 +1,56 @@
 import React from 'react'
 
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+import { addHours} from 'date-fns'
+import { Navbar } from '../components/Navbar'
+import { localizer, getMesagesEs } from '../../helpers'
+
+
+
+
+const events = [{
+  title: 'Cumpleanios del jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Eden'
+  }
+
+}]
+
 export const CalendarPage = () => {
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+    return {
+      style
+    }
+  }
   return (
-    <div>CalendarPage</div>
+    <>
+      <Navbar/>
+
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMesagesEs()}
+        eventPropGetter={eventStyleGetter}
+      />
+    </>
   )
 }
